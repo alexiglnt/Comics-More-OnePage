e9f0c367dd5c7304cbf8339178c02ea4e6b6c660
alexiglnt <alexigallonet@gmail.com>
2023-03-24 13:03:50 +0100

    Modifications

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,9 +15,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>
-          TEST
-        </h1>
+
+        {/* GITHUB CORNER */}
+        <a href="https://github.com/alexiglnt/comics-more" className="github-corner"
+          aria-label="View source on GitHub"><svg width="80" height="80" viewBox="0 0 250 250"
+            style={{ fill: '#151513', color: '#fff', position: 'absolute', top: '0', border: '0', right: '0' }} aria-hidden="true">
+            <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+            <path
+              d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
+              fill="currentColor" style={{ transformOrigin: '130px 106px' }} className="octo-arm"></path>
+            <path
+              d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
+              fill="currentColor" className="octo-body"></path>
+          </svg></a>
+        <h1 className='title' > COMICS MORE </h1>
+
+        <p>
+          Comics More est un site en ligne dédié à la lecture de comics, tels que MARVEL et DC Comics. Le site utilise un système de crédits pour l'achat de ces bandes dessinées, chaque comics étant évalué en fonction de son nombre de pages. Les utilisateurs reçoivent 10 crédits gratuits lors de leur inscription. Ils ont le choix entre deux modes de paiement pour acheter des crédits : par carte bancaire ou via un compte PayPal. Une fois les crédits achetés, les utilisateurs peuvent retrouver leurs comics dans leur bibliothèque personnelle. Ils peuvent effectuer des recherches de comics en utilisant la barre de recherche ou en explorant les différentes collections de personnages. Par exemple, la collection Spider-Man regroupe tous les comics liés à ce personnage, qui est publié sous la maison d'édition Marvel. Les utilisateurs peuvent également ajouter des comics à leurs favoris.
+        </p>
+
+        <p>
+          Comics More utilise les Frameworks Vue.js et Symfony pour développer le site, stockant les informations dans une base de données. Les images des comics, quant à elles, sont stockées sur Amazon Web Services (AWS). La base de données contient toutes les informations liées aux comics ainsi qu'aux utilisateurs. Le site est relié à la base de données via une API développer en PHP avec Symfony, qui fait le lien entre le back-end et le front-end.
+        </p>
+
+        <p>
+          Pour l'authentification des utilisateurs, Comics More utilise le système lexik-jwt-authentication avec des tokens qui expirent après un certain temps. Les mots de passe des utilisateurs sont cryptés. Les administrateurs ont accès à des fonctionnalités spéciales, tels que la lecture gratuite de tous les comics et la possibilité de naviguer sur tout le site sans utiliser de crédits.
+        </p>
+
       </main>
     </>
   )
